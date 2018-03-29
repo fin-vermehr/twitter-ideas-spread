decc7f6d73cabce977c2bf5f889d420408a967a4
quentinv72 <quentin.vilchez@gmail.com>
2018-03-28 22:15:39 -0400

    Slides

--- data fitting/process_data.py
+++ data fitting/process_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df  = pd.read_csv('CambridgeAnalytica_processed.csv')
+df  = pd.read_csv('DeleteFacebook_processed.csv')
 df = df.drop(['Unnamed: 0','screen_name', 'text'], axis = 1)
 df.time = pd.to_datetime(df['time'], format = '%Y-%m-%d %H:%M:%S', errors = 'coerce')
 df = df.dropna()
@@ -8,4 +8,4 @@ index = pd.DatetimeIndex(df.time)
 ts = pd.Series(df.cumulative_tweets.values, index=index)
 ts = ts[~ts.index.duplicated(keep='first')]
 converted = ts.asfreq('15min', method='pad')
-converted.to_csv(path= 'CambridgeAnalytica.csv', header = False, index = False)
+converted.to_csv(path= 'DeleteFacebook.csv', header = False, index = False)
